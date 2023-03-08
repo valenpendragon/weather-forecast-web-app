0918bc02e237401c7f64b5a31b2a0fa6f0c3a0f0
Jeffrey Scott <valenpendragon@gmail.com>
2023-03-08 04:20:09 -0500

    Add data filtering to get_data function. Add backend.py to store backend functions.

---
+++ backend.py
@@ -0,0 +1,32 @@
+import os
+import requests
+
+
+def get_data(place: str, forecast_days=1, data_type="Temperature"):
+    """This function uses place to determine the location for the weather
+    data, forecast_days for the number of days to return, and data_type
+    for the structure or appearance of the data: graphical p[otting of
+    the expected temperatures or icons for the type of weather, such as
+    partly cloudy, rain, etc. This data is accumulated and returned to the
+    calling program.
+    :return: data
+    """
+    #api_key = os.getenv("WMORG_API_KEY")
+    api_key = "66717d17d42c5f3711a5e0aa863c6d1f"
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}" \
+          f"&appid={api_key}&units=imperial"
+    response = requests.get(url)
+    data = response.json()
+    no_data_values = 8 * forecast_days
+    filtered_data = data["list"][:no_data_values]
+    temps = [dict["main"]["temp"] for dict in filtered_data]
+    conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
+    if data_type == "Temperature":
+        return temps
+    else:
+        return conditions
+
+
+if __name__ == "__main__":
+    print(get_data("pickerington", forecast_days=3, data_type="Temperature"))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for Up to Five Days")
 place = st.text_input("Place:", help="Type the city name")
@@ -9,14 +10,8 @@ option = st.selectbox("Select data to view",
                       options=("Temperature", "Sky"))
 st.subheader(f"{option} for the following {days} day(s) in {place}")
 
+data = get_data(place, days, option)
 
-def get_data(days):
-    dates = ["2023-03-05", "2023-03-06", "2023-03-07"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
-d, t = get_data(days)
+# d, t = get_data(days)
 figure = px.line(x=d, y=t, labels={"x": "Data", "y": "Temperature (C)"})
 st.plotly_chart(figure)
